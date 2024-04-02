816f0543803aedba956302e8036e9b1924c7ebb9
eve <eve.ye@great-good.tw>
2024-04-02 10:09:17 +0800

    mergy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\News;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,12 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        //User::factory(10)->create();
+
+        News::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password'=> bcrypt('g123456'),
+            'password' => bcrypt('g123456'),
         ]);
     }
 }
